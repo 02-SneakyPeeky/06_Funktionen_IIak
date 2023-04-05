68188591f86ff236d3af6969ca512f90801a8da8
ASK-DONE <knueppelalex@web.de>
2023-04-05 11:08:51 +0200

    Grundrechenarten

--- script_01.js
+++ script_01.js
@@ -11,13 +11,62 @@
 4. Ausgabe in Konsole : 
 */
 
-/
+
 
 
 // Konstanten
 const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
 const ERROR_STR_GEN = "Irgendetwas ging schief!"
 
+
+// module: division a / b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(3,0));
+
+
+function divide(a,b) {
+	
+	// if (b != 0) {            // Variante 1
+	// return a / b;	        // ""
+ 	// } else {                 // ""
+	// 	return ERROR_STR_DIV    // ""
+	// }                        // ""
+	// return a / b;            // ""
+
+	if (b == 0) {            // Variante 2
+	return ERROR_STR_DIV     // ""
+	}                        // ""
+	return a / b;            // ""
+}
+
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+
+function multiply(a,b) {
+	return a * b
+}
+
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+
+function subtract(a,b) {
+	return a - b; 	
+}
+
+// module: addition a + b |  test:
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	return a + b;
+}
 
 
 // module: output | test:
